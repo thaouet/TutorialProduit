ec1f6663d1b77dc2edbca3d24557e5e547ec1a9b
thaouet <thaouet@outlook.com>
2017-05-04 21:16:49 +0100

    Ajoutez des fichiers projet.

---
+++ TutorialProduit/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace TutorialProduit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.textBoxPrix = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridProduit = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.prix = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProduit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "connexion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "state";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Inserer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(70, 105);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(100, 20);
+            this.textBoxId.TabIndex = 3;
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(70, 142);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNom.TabIndex = 4;
+            // 
+            // textBoxPrix
+            // 
+            this.textBoxPrix.Location = new System.Drawing.Point(70, 182);
+            this.textBoxPrix.Name = "textBoxPrix";
+            this.textBoxPrix.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrix.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "nom";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "prix";
+            // 
+            // dataGridProduit
+            // 
+            this.dataGridProduit.AllowUserToAddRows = false;
+            this.dataGridProduit.AllowUserToDeleteRows = false;
+            this.dataGridProduit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridProduit.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.nom,
+            this.prix});
+            this.dataGridProduit.Location = new System.Drawing.Point(282, 61);
+            this.dataGridProduit.Name = "dataGridProduit";
+            this.dataGridProduit.ReadOnly = true;
+            this.dataGridProduit.Size = new System.Drawing.Size(350, 150);
+            this.dataGridProduit.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(282, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Afficher";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // nom
+            // 
+            this.nom.HeaderText = "NOM";
+            this.nom.Name = "nom";
+            this.nom.ReadOnly = true;
+            // 
+            // prix
+            // 
+            this.prix.HeaderText = "PRIX";
+            this.prix.Name = "prix";
+            this.prix.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridProduit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPrix);
+            this.Controls.Add(this.textBoxNom);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProduit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.TextBox textBoxPrix;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridProduit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn prix;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TutorialProduit/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace TutorialProduit
+{
+
+
+    
+
+    public partial class Form1 : Form
+    {
+
+
+        SqlConnection connexion;
+        SqlCommand insertCommand;
+        SqlCommand selectCommand;
+        public Form1()
+        {
+            InitializeComponent();
+            connexion = new SqlConnection("Server=COLOSSUS;Database=ProduitDB;User Id=sa;Password = zilla; ");
+            connexion.StateChange += new StateChangeEventHandler(Connexion_StateChange);
+
+
+            insertCommand = new SqlCommand();
+            insertCommand.Connection = connexion;
+            insertCommand.Parameters.Add(new SqlParameter("@id", SqlDbType.Int));
+            insertCommand.Parameters.Add(new SqlParameter("@nom", SqlDbType.VarChar));
+            insertCommand.Parameters.Add(new SqlParameter("@prix", SqlDbType.Decimal));
+            insertCommand.CommandText = "insert into PRODUIT values (@id,@nom,@prix)";
+
+            selectCommand = new SqlCommand("Select IdProduit, Libelle, Prix from PRODUIT",connexion);
+                       
+           
+
+        }
+
+        private void Connexion_StateChange(object sender, StateChangeEventArgs e)
+        {
+            label1.Text = "state: " + connexion.State.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connexion.State == ConnectionState.Closed)
+                {
+                    connexion.Open();
+                   
+                }
+                else
+                    if (connexion.State == ConnectionState.Open)
+                {
+                    connexion.Close();
+                   
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERREUR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                connexion.Open();
+                insertCommand.Parameters["@id"].Value = int.Parse(textBoxId.Text);
+                insertCommand.Parameters["@nom"].Value = textBoxNom.Text;
+                insertCommand.Parameters["@prix"].Value = Double.Parse(textBoxPrix.Text);
+
+                int i = insertCommand.ExecuteNonQuery();
+                MessageBox.Show(i + " enregestrements insérés", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                connexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERREUR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridProduit.Rows.Clear();
+            connexion.Open();
+
+            SqlDataReader reader = selectCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                
+                object[] ligne = { reader.GetInt32(0), reader[1].ToString(), decimal.Parse(reader[2].ToString())};
+
+                dataGridProduit.Rows.Add(ligne);
+            }
+
+            connexion.Close();
+
+
+        }
+    }
+}
